cbf1c4cc123656a21ecf340dfd763df7f33e4f2a
cemalmehmetcam <cemalmehmetcam@gmail.com>
2025-12-24 19:43:09 +0300

    son düzeltmeler.

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d843caea31b762c744827109d28cc5325089d28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a1d5222645d738aa7f14045112183b77b0b441")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
